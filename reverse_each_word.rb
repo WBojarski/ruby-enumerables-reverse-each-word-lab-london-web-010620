5a1976c6f569a02aaf80b0438e1be134f8e9e8df
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-28 19:48:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,13 @@ def reverse_each_word(sentence)
 	new_separate.join(' ')
 end
 
+def reverse_each_word(sentence)
+
+	separate = sentence.split
+	new_separate = separate.collect do |word|
+		word.reverse
+	end
+	new_separate.join(' ')
+end
+
+
